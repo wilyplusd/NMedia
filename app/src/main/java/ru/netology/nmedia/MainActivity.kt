977214c249a422e9e2ab54bd70b42ee7b12cb7e9
Irina Derevyanko <wily@list.ru>
2023-08-08 13:53:17 +0300

    Set constraint layout

---
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -0,0 +1,20 @@
+package ru.netology.nmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            println(resources.displayMetrics.heightPixels) // 1794
+            println(resources.displayMetrics.widthPixels) // 1080
+            println(resources.displayMetrics.densityDpi) // 420
+            println(resources.displayMetrics.density) // 2.625
+        }
+
+}
