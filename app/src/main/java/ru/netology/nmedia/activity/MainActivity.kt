381c68d84a0c4ddb5a65696f079d6864ae73b8b0
Irina Derevyanko <wily@list.ru>
2024-01-09 23:57:32 +0300

    Change edit

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,10 +3,10 @@ package ru.netology.nmedia.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.View.GONE
+
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.core.view.isVisible
+
 import ru.netology.nmedia.R
 import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             override fun onLike(post: Post) {
                 viewModel.like(post.id)
             }
+
             override fun onShare(post: Post) {
                 viewModel.share(post.id)
             }
@@ -50,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                 return@observe
             }
 
-          binding.groupEditView.visibility =  View.VISIBLE
+            binding.groupEditView.visibility = View.VISIBLE
             binding.contentPreviewLabel.setText(post.content)
 
             with(binding.content) {
@@ -58,13 +59,17 @@ class MainActivity : AppCompatActivity() {
                 setText(post.content)
 
             }
+
+
         }
         binding.cancelButton.setOnClickListener {
-           binding.groupEditView.visibility =  View.GONE
+            binding.groupEditView.visibility = View.GONE
             with(binding.content) {
                 setText("")
                 clearFocus()
                 AndroidUtils.hideKeyboard(this)
+                viewModel.save()
+
             }
 
         }
@@ -85,7 +90,7 @@ class MainActivity : AppCompatActivity() {
                 setText("")
                 clearFocus()
                 AndroidUtils.hideKeyboard(this)
-               binding.groupEditView.visibility =  View.GONE
+                binding.groupEditView.visibility = View.GONE
             }
         }
     }
